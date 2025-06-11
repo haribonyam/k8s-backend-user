f3ee076e3692e3ec0dd765be57f7f86fdde0a8f9
하리보냠 <drppr99@gmail.com>
2025-06-11 16:17:19 +0900

    fix

--- src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
+++ src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
@@ -2,8 +2,10 @@ package com.welab.k8s_backend_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class K8sBackendUserApplication {
 
 	public static void main(String[] args) {

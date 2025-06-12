f73b9af24fc3f297b8c6844cd788b663d2b6edb8
하리보냠 <drppr99@gmail.com>
2025-06-12 14:31:26 +0900

    v0.0.6

---
+++ src/main/java/com/welab/k8s_backend_user/service/SiteUserService.java
@@ -0,0 +1,27 @@
+package com.welab.k8s_backend_user.service;
+
+import com.welab.k8s_backend_user.domain.SiteUser;
+import com.welab.k8s_backend_user.dto.SiteUserRegisterDto;
+import com.welab.k8s_backend_user.dto.event.SiteUserInfoEvent;
+import com.welab.k8s_backend_user.event.producer.KafkaMessageProducer;
+import com.welab.k8s_backend_user.repository.SiteUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SiteUserService {
+    private final SiteUserRepository siteUserRepository;
+    private final KafkaMessageProducer kafkaMessageProducer;
+
+    @Transactional
+    public void registerUser(SiteUserRegisterDto registerDto) {
+        SiteUser siteUser = registerDto.toEntity();
+        siteUserRepository.save(siteUser);
+        SiteUserInfoEvent event = SiteUserInfoEvent.fromEntity("Create", siteUser);
+        kafkaMessageProducer.send(SiteUserInfoEvent.Topic, event);
+    }
+}
